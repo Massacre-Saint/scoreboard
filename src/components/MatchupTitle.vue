185fbc2b65b9c0c391c1c964522a6c22e6d04cf0
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2024-01-04 20:05:17 -0600

    Added scorecard functionality and dynamic styling to MatchupTitle Componenet

--- src/components/MatchupTitle.vue
+++ src/components/MatchupTitle.vue
@@ -3,7 +3,23 @@ const props = defineProps(['blueTeam', 'redTeam']);
 </script>
 
 <template>
-  <h1 class="is-size-1">{{ props.blueTeam.name }} vs. {{ props.redTeam.name }}</h1>
+  <h1>
+    <span class="team-1" :class="{ winning: blueTeam.score > redTeam.score }">
+      {{ props.blueTeam.name }}
+    </span>
+    <span> vs. </span>
+    <span class="team-2" :class="{ winning: redTeam.score > blueTeam.score }">
+      {{ props.redTeam.name }}
+    </span>
+  </h1>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  font-size: 8vw;
+  margin-bottom: 0;
+}
+.winning {
+  color: goldenrod;
+}
+</style>
