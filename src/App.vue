c71627a3ad8c9c5ef467973b842f4258a7ebeb80
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2024-01-06 17:46:27 -0600

    Added parent component and stying to app

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,9 @@
 <script setup>
-import { ref } from 'vue';
-import MatchupTitle from './components/MatchupTitle.vue';
-import ScoreCard from './components/ScoreCard.vue';
-import GameStatus from './components/GameStatus.vue';
-
-const blueTeam = ref({name: 'Blue Team', score: 0});
-const redTeam = ref({name: 'Red Team', score: 0});
-
-const increment = (team) => {
-  team.score++
-};
-const decrement = (team) => {
-  if (team.score > 0) {
-    team.score--
-  };
-};
+import ScoreBoard from './components/ScoreBoard.vue';
 </script>
 
 <template>
-  <MatchupTitle :blue-team="blueTeam" :red-team="redTeam"/>
-  <GameStatus :blue-team="blueTeam" :red-team="redTeam"/>
-  <ScoreCard
-    :score="blueTeam.score"
-    @increment="increment(blueTeam)"
-    @decrement="decrement(blueTeam)"
-  />
-  <ScoreCard
-    :score="redTeam.score"
-    @increment="increment(redTeam)"
-    @decrement="decrement(redTeam)"
-  />
+  <ScoreBoard />
 </template>
 
 <style scoped>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from 'vue';
+import MatchupTitle from "./Matchuptitle.vue"
+import GameStatus from "./GameStatus.vue"
+import ScoreCard from './ScoreCard.vue';
+const blueTeam = ref({name: 'Blue Team', score: 0});
+const redTeam = ref({name: 'Red Team', score: 0});
+
+const increment = (team) => {
+  team.score++
+};
+const decrement = (team) => {
+  if (team.score > 0) {
+    team.score--
+  };
+};
+</script>
+
+<template>
+  <div id="app">
+    <MatchupTitle :blue-team="blueTeam" :red-team="redTeam"/>
+    <GameStatus :blue-team="blueTeam" :red-team="redTeam"/>
+    <div class="scores">
+      <ScoreCard
+        :score="blueTeam.score"
+        @increment="increment(blueTeam)"
+        @decrement="decrement(blueTeam)"
+      />
+      <ScoreCard
+        :score="redTeam.score"
+        @increment="increment(redTeam)"
+        @decrement="decrement(redTeam)"
+        />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: rgba(255, 255, 255, 0.8);
+  margin-top: 60px;
+}
+.scores {
+  display: flex;
+  justify-content: space-evenly;
+}</style>
